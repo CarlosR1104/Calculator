175eb2d8a9414dbb43f815b2588efa7e53aee856
ermen <stivencv7@gmail.com>
2021-08-06 14:33:23 -0500

    feat:realice tarea ES

---
+++ Calcular.py
@@ -0,0 +1,7 @@
+
+def divicion (nume1,nume2):
+    nume1=float(input('ingrese el numero1: '))
+    nume2=float(input('ingrese el numero2: '))
+    resultado=nume1/nume2
+    print(resultado)
+divicion()
