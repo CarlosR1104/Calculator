ce9bd4135eb21124dd12f1e0de0feb41ebb8c469
JhonJairoChala <88052012+JhonJairoChala@users.noreply.github.com>
2021-08-06 14:39:28 -0500

    Add files via upload

---
+++ Calcular.py
@@ -0,0 +1,8 @@
+def suma():
+    num1=float(input("Ingrese el numero: "))
+    num2=float(input("Ingrese el numero: "))
+    suma=num1+num2
+    print ("El resultado de la suma es: ")
+suma()
+    
+    
