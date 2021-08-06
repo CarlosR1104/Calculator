c0dfd1aa96eb7d1367b50672a46888d292b70066
Jaider Suaza <jaidersuazag@gmail.com>
2021-08-06 14:32:31 -0500

    crear la resta

---
+++ Calcular.py
@@ -0,0 +1,10 @@
+# jaider crear la funcion de restar 
+def resta():
+
+   num1:float(input("ingrese el numero:"))
+   num2:float(input("ingrese el segundo:"))
+   resta=num1-num2
+   print(resta)
+   print("la resta es :",resta)
+
+resta()
